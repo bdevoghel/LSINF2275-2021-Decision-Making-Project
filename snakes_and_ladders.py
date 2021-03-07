df91715c2e52fd43d5b8e73a1eee4e43e1ace54f
bdevoghel <bdev@telenet.be>
2021-03-07 17:06:36 +0100

    initial commit

---
+++ snakes_and_ladders.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+test_layout1 = np.array([0 for _ in range(15)])
+
+
+def markovDecision(layout, circle):
+    pass
+
+
+if __name__ == '__main__':
+    print(markovDecision(test_layout1, circle=False))
