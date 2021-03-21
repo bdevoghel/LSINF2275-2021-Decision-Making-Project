3b9033e53709e42f553ca67637c3ad92f3c4f4c7
bdevoghel <bdev@telenet.be>
2021-03-21 18:41:16 +0100

    debug and  clean up

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -24,18 +24,27 @@ PENALTY = 2
 PRISON = 3
 GAMBLE = 4
 
-# layout 1 : only ordinary squares, no traps
-test_layout1 = np.array([0 for _ in range(15)])
+# only ORDINARY squares, no traps
+layout_ORDINARY = np.array([0 for _ in range(15)])
+
+# only PENALTY (go back 3) traps
+layout_PENALTY = np.ones(15) * PENALTY
+layout_PENALTY[[0, -1]] = 0  # start and goal squares must be ordinary
+
+# only PRISON (stay one turn) traps
+layout_PRISON = np.ones(15) * PRISON
+layout_PRISON[[0, -1]] = 0  # start and goal squares must be ordinary
 
 # layout 2 : random initialized traps
 test_layout2 = np.random.randint(low=0, high=5, size=15)
 test_layout2[[0, -1]] = 0  # start and goal squares must be ordinary
 
-# layout 3 : perso
+# layout 3 : personalized
 test_layout3 = np.array([ORDINARY for _ in range(15)])
 test_layout3[8] = RESTART
 test_layout3[7] = GAMBLE
 test_layout3[12] = PENALTY
+
 
 
 class Die:
@@ -70,7 +79,7 @@ class Board:
         self.circle = circle
 
         # transition matrices
-        self.P = {die:np.array(self.compute_transition_matrix(die)) for die in self.dice}
+        self.P = {die: np.array(self.compute_transition_matrix(die)) for die in self.dice}
 
     def __repr__(self):
         return f"[Board:{list(self.layout)}-Circle:{self.circle}]"
@@ -83,6 +92,10 @@ class Board:
         return tm
 
     def accessible_square(self, pos, delta, budget):
+        """
+        Returns a list of tuples of the accessible squares with the budget distributed
+        according to the squares' respective landing probabilities
+        """
         if pos < 2:
             return [(pos+delta, budget)]
         elif pos == 2:
@@ -152,6 +165,7 @@ class Board:
         """
         Returns vector with landing probabilities on each square
         :param start_position: must be in [0, 14]
+        :param die: selected die
         """
         landing_proba = np.zeros(len(self.layout))
         for step in die.possible_steps:
@@ -166,7 +180,7 @@ class Board:
                     landing_proba[self.apply_penalty(new_pos)] += die.trap_trigger_proba * budget
                     landing_proba[new_pos] += (1 - die.trap_trigger_proba) * budget
                 elif square_type == PRISON:
-                    pass  # TODO
+                    landing_proba[new_pos] += budget  # TODO modify ??
                 elif square_type == GAMBLE:
                     landing_proba[range(len(self.layout))] += 1 / len(self.layout)
 
@@ -180,37 +194,44 @@ def markovDecision(layout: np.ndarray, circle: bool):
     :param circle: indicates if the player must land exactly on the final square to win or still wins by overstepping
     """
     board = Board(layout, circle)
-    actions = np.zeros(len(board.layout) - 1)
+    actions = - np.ones(len(board.layout) - 1)
     costs = - np.ones(len(board.layout))
-    costs[-1] = 0
+    costs[-1] = 0  # goal square
 
-    # Value-iteration algo
-    def rec(pos, previous_die, budget):
-        if costs[pos] != -1:
-            return budget * costs[pos]
-        elif budget < 1e-12:
+    def value_iteration(pos, previous_die, budget):
+        """Computes estimated cost of reaching goal with the Value Iteration algorithm"""
+        if costs[pos] != -1:  # cost already computed previously
+            return costs[pos] * budget  # TODO sure ??
+        elif budget < 1e-12:  # budget is negligible
             return 0
-        cost = 2 if pos == PRISON else 1
-        action_costs = {}
+
+        cost = 2 if board.layout[pos] == PRISON else 1  # TODO check if prison was triggered
+        cost_per_die = {}
+
         for die in board.dice:
-            total_cost = 0
-            for state, prob in enumerate(board.P[die][pos]):
-                if prob > 0 and state != pos:
-                    total_cost += rec(state, die, budget*prob)
-            action_costs[die] = cost + total_cost
-            action_costs[die] /= 1 - board.P[die][pos, pos]
-        min_action = min(action_costs, key=action_costs.get)
-        costs[pos] = action_costs[min_action]
-        actions[pos] = min_action.type
+            cost_per_die[die] = cost
+
+            # add subjacent costs
+            for next_pos, landing_proba in enumerate(board.P[die][pos]):
+                if landing_proba > 0 and next_pos != pos:
+                    next_budget = budget * landing_proba
+                    cost_per_die[die] += value_iteration(next_pos, die, next_budget)
+
+            if board.P[die][pos, pos] != 1:
+                cost_per_die[die] /= 1 - board.P[die][pos, pos]  # p(k|k)
+
+        cheapest_die = min(cost_per_die, key=cost_per_die.get)
+        costs[pos] = cost_per_die[cheapest_die]
+        actions[pos] = cheapest_die.type
         return costs[pos] * budget
 
-    for i in range(len(actions) - 1):
-        costs[i] = rec(i, None, 1)
+    for square in range(len(actions)):
+        costs[square] = value_iteration(square, previous_die=None, budget=1)
 
     return [costs[:-1], actions]
 
 
-def test_markovDecision(layout, circle):
+def test_markovDecision(layout, circle, name=""):
     assert isinstance(layout, np.ndarray) and len(layout) == 15, f"Input layout is not a ndarray or is not of length 15"
     assert isinstance(circle, bool), f"Input circle is not a bool"
 
@@ -224,10 +245,13 @@ def test_markovDecision(layout, circle):
     assert isinstance(dice, np.ndarray) and len(dice) == 14, \
         f"Output dice is not a ndarray or is not of length 14\n\nDICE : {dice}"
 
-    print(f"Success\n\nEXPEC : {expec}\nDICE  : {dice}")
+    _format = "{:<7}"*14
+    print(f"\nSuccess {name}\n        {_format.format(*list(range(1,15)))}\nEXPEC : {_format.format(*np.around(expec, 2))}\nDICE  : {_format.format(*np.around(dice, 2))}")
 
 
 if __name__ == '__main__':
-    test_markovDecision(test_layout3, False)
+    test_markovDecision(layout_ORDINARY, True, "ORDINARY")
+    test_markovDecision(layout_PRISON, True, "PRISON")
+    test_markovDecision(layout_PENALTY, True, "PENALTY")
 
     # Bonus : implement empirical tests to show convergence towards obtained results
